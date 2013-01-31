43ab035b280952958fe8f80ff339e819ca0944d5
Fabian Irsara <info@fabianirsara.com>
2013-01-31 15:10:34 +0100

    dynamically adjust span-size based on device

--- assets/javascripts/app/global.js
+++ assets/javascripts/app/global.js
@@ -6,10 +6,24 @@ app.global.setup = function(){
 
 app.global.match = function(){
   console.log('match: ' + app.is);
+
+  // Adjust Grid-Layout based on data-attribute
+  $('[data-span-'+app.is+']').each(function(){
+    $(this).removeClass('span0 span1 span2 span3 span4 span5 span6 span7 span8 span9 span10 span11 span12');
+    $(this).addClass('span' + $(this).attr('data-span-'+app.is).replace('span', ''));
+  });
+
+  // dynamically add classes based on data-attribute
+  $('[data-classes-'+app.is+']').each(function(){
+    $(this).addClass($(this).attr('data-classes-'+app.is));
+  });
 };
 
 app.global.unmatch = function(){
   console.log('unmatch: ' + app.was);
+  
+  // dynamically remove classes based on data-attribute
+  $('[data-classes-'+app.was+']').each(function(){
+    $(this).removeClass($(this).attr('data-classes-'+app.was));
+  });
 };
-
-
