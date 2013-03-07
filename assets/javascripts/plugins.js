4dc8224c25c698eac11ff194c0bd27f5f7fe4c5d
Fabian Irsara <info@fabianirsara.com>
2013-03-07 10:21:32 +0100

    add requestAnimationFrame for all browsers

--- assets/javascripts/plugins.js
+++ assets/javascripts/plugins.js
@@ -21,6 +21,23 @@
     }
 }());
 
+if ( !window.requestAnimationFrame ) {
+
+  window.requestAnimationFrame = ( function() {
+
+    return window.webkitRequestAnimationFrame ||
+      window.mozRequestAnimationFrame ||
+      window.oRequestAnimationFrame ||
+      window.msRequestAnimationFrame ||
+      function( callback ) {
+
+      window.setTimeout( callback, 1000 / 60 );
+
+    };
+
+  } )();
+}
+
 // Place any jQuery/helper plugins in here.
 
 
