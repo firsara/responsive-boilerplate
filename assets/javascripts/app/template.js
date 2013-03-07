a64931793b6fd63c144dcc4d360c7031b6ccf3ba
Fabian Irsara <info@fabianirsara.com>
2013-03-07 11:32:51 +0100

    add default template-controller

---
+++ assets/javascripts/app/template.js
@@ -0,0 +1,21 @@
+app.template = {};
+
+app.template.before = function(){
+  $('body').find('*').unbind().off();
+};
+
+app.template.parse = function(data){
+  var htmlString = data;
+  var html = $(htmlString);
+  
+  $('body').html(html.html());
+
+  $('.dynamic-input').remove();
+  app.global.setup();
+  app.global.match();
+  app[app.is].match();
+
+  $(window).trigger('resize');
+  app.global.resize();
+  app[app.is].resize();
+};
