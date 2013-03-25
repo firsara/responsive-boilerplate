1dba306d5927bf0ae178def9f9aeeb1d2ab27a25
Fabian Irsara <info@fabianirsara.com>
2013-03-25 17:17:15 +0100

    add wrapInChunks plugin

--- assets/javascripts/plugins.js
+++ assets/javascripts/plugins.js
@@ -38,6 +38,26 @@ if ( !window.requestAnimationFrame ) {
   } )();
 }
 
+
+
+jQuery.fn.wrapInChunks = function(html, chunkSize) {
+  chunkSize = chunkSize || 1;
+  var items = this.get(),
+      rows = [],
+      cur = rows[0] = $(html);
+
+  while (items[0]) {
+    if (rows[rows.length - 1].children().length === chunkSize) {
+      cur = rows[rows.length] = $(html);
+    }
+
+    cur.append( items.shift() );
+  }
+
+  return this.pushStack(rows);
+};
+
+
 // Place any jQuery/helper plugins in here.
 
 
