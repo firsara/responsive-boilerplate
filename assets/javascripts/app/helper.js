967fee2bb617dfcb02dde1fe1675d2ede163aaa7
Fabian Irsara <info@fabianirsara.com>
2013-05-02 21:38:49 +0200

    cleanup, add helper methods

--- assets/javascripts/app/helper.js
+++ assets/javascripts/app/helper.js
@@ -5,22 +5,52 @@ app.helper = (function(window, document, $, self, undefined){
   (function(){
     self.detections.isTouchDevice = ('ontouchstart' in window || 'onmsgesturechange' in window);
     self.detections.supportsHistory = (window.history.pushState !== undefined);
+    self.detections.isRetina = (window.devicePixelRatio > 1);
+    self.detections.supportsTransitions = (function(){
+      var v = ['ms', 'Khtml', 'O', 'Moz', 'Webkit', ''];
+      while( v.length ) if( v.pop() + 'Transition' in document.body.style ) return true;
+      return false;
+    })();
+
+    // Hack, hack, hack :)
+    // Returns the real elements to scroll (supports window/iframes, documents and regular nodes)
+    $.fn._scrollable = function(){
+      return this.map(function(){
+        var elem = this,
+          isWin = !elem.nodeName || $.inArray( elem.nodeName.toLowerCase(), ['iframe','#document','html','body'] ) != -1;
+
+          if( !isWin )
+            return elem;
+
+        var doc = (elem.contentWindow || elem).document || elem.ownerDocument || elem;
+        
+        return $.client.browser == 'Safari' || BrowserDetect.webkit || doc.compatMode == 'BackCompat' ?
+          doc.body : 
+          doc.documentElement;
+      });
+    };
+
+    self.detections.scrollable = $(window)._scrollable();
   })();
 
-  self.scrollTo = function(pos, speed, holder){
+  self.scrollTo = function(pos, speed, ease, holder){
     if (pos === null) return;
+    if (ease === null) ease = 'linear';
     if (! speed) speed = 0;
 
     if (holder) {
       return $(holder).animate({scrollTop: pos}, 1);
     }
 
-    $('html, body').animate({scrollTop: pos}, speed);
+    if (speed == 0) {
+      self.detections.scrollable.css({scrollTop: pos});
+    } else {
+      self.detections.scrollable.animate({scrollTop: pos}, speed, ease);
+    }
   };
 
   self.getScrollTop = function(){
-    var scrollable = $(window)._scrollable();
-    return scrollable.scrollTop();
+    return self.detections.scrollable.scrollTop();
   };
 
   self.resizeFor = function(timeoutTime){
@@ -43,20 +73,16 @@ app.helper = (function(window, document, $, self, undefined){
   };
 
   self.isRetina = function(){
-    return window.devicePixelRatio > 1;
+    return self.detections.isRetina;
   };
 
   self.supportsHistory = function(){
     return app.helper.detections.supportsHistory;
   };
 
-  self.supportsTransitions = (function(){
-    var v = ['ms', 'Khtml', 'O', 'Moz', 'Webkit', ''];
-    // Tests for vendor specific prop
-    while( v.length )
-      if( v.pop() + 'Transition' in document.body.style ) return function(){ return true; };
-    return function(){ return false; };
-  })();
+  self.supportsTransitions = function(){
+    return app.helper.detections.supportsTransitions;
+  };
 
   self.url = function(url){
     if (! (app.address && app.address.ready && $.address)) {
